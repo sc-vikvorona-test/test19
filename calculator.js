4d3174a63056517f101322e5e5ef62c06304ac1a
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-03-17 11:46:29 +0100

    add calculator with bugs

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function divide(a, b) {
+  return a / b;
+}
+
+function toUpperCase(str) {
+  return str.tolowercase();
+}
+
+function getUser(id) {
+  const users = { 1: "Alice", 2: "Bob" };
+  return users[id];
+}
+
+module.exports = { divide, toUpperCase, getUser };
